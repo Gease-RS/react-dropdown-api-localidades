4a5340659e91d2c93c487ca4378b7a382408f431
Gease-RS <geasebrasil@gmail.com>
2022-01-20 19:24:22 -0300

    Commit Inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form>
+        <label>Estado:</label>
+        <select>
+          <option value="">Selecione um Estado</option>
+        </select>
+        <label>Cidade:</label>
+        <select>
+          <option value="">Selecione uma cidade</option>
+        </select>
+      </form>
     </div>
   );
 }
